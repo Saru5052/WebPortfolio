17b78b1772fb5cfa72d5d9540a31f53e986e9359
Saru5052 <160249529+Saru5052@users.noreply.github.com>
2025-02-28 20:14:37 +0530

    Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         <li><a href="#skills">Skills</a></li>
         <li><a href="#projects">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
-        <li><a href="./resume.pdf" download>Resume</a></li>
+        <li><a href="/resume.pdf" download>Resume</a></li>
        
         
       </ul>
